0e2f958dcc6d0243ed9cd991eb77156eb4342039
superterminal <trendemil@gmail.com>
2019-11-12 23:46:42 +0200

    added about and basic routing

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import React, { Component } from 'react';
 import uuid from 'uuid';
+import { BrowserRouter as Router, Route} from 'react-router-dom';
 
 import Todos from './components/Todos';
 import Header from './components/layout/header';
 import AddTodo from './components/AddTodo';
+import About from './components/pages/about';
 
 import './App.css';
 
@@ -56,15 +58,22 @@ class App extends Component {
 
   render() {
     return (
-      <div className="App">
-        <div className="container">
-          <Header />
-          <AddTodo addTodo={this.addTodo}/>
-          <Todos todos={this.state.todos} 
-          markComplete={this.markComplete} 
-          deleteTodo={this.deleteTodo}/>
+      <Router>
+        <div className="App">
+          <div className="container">
+            <Header />
+            <Route exact path="/" render={props => (
+              <React.Fragment>
+                <AddTodo addTodo={this.addTodo}/>
+                <Todos todos={this.state.todos} 
+                markComplete={this.markComplete} 
+                deleteTodo={this.deleteTodo}/>
+              </React.Fragment>
+            )} />
+            <Route path="/about" component={About} />
+          </div>
         </div>
-      </div>
+      </Router>
     )
   }
 }
