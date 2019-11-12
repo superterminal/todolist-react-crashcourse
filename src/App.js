6f59601f6c2a6031d4df2f413678349b64cf03a3
superterminal <trendemil@gmail.com>
2019-11-12 23:05:23 +0200

    added sample todos and working completed/not completed logic

--- src/App.js
+++ src/App.js
@@ -1,11 +1,44 @@
 import React, { Component } from 'react';
+import Todos from './components/Todos';
+
 import './App.css';
 
 class App extends Component {
+
+  state = {
+    todos: [
+      {
+        id: 1,
+        title: 'take out the trash',
+        completed: false
+      },
+      {
+        id: 2,
+        title: 'play video games',
+        completed: true
+      },
+      {
+        id: 3,
+        title: 'do your homework',
+        completed: false
+      }
+    ]
+  }
+
+  
+  markComplete = (id) => {
+      this.setState({ todos: this.state.todos.map(todo => {
+        if (todo.id === id) {
+          todo.completed = !todo.completed;
+        }
+        return todo;
+      })})
+  }
+
   render() {
     return (
       <div className="App">
-        <h1>App</h1>
+        <Todos todos={this.state.todos} markComplete={this.markComplete} />
       </div>
     )
   }
